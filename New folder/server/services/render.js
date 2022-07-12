0f065a5bc6b87f82787307c084e031c1dcc9dfbb
Rati Sonkar <73036667+rati630@users.noreply.github.com>
2022-07-12 14:30:04 +0530

    Add files via upload

---
+++ New folder/server/services/render.js
@@ -0,0 +1,12 @@
+
+exports.homeRoutes=(req,res)=>{
+    res.render('index',{users: "New Data"});
+}
+
+exports.add_user=(req,res)=>{
+    res.render('add_user');
+}
+
+exports.update_user=(req,res)=>{
+    res.render('update_user');
+}
