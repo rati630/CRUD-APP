c41e9e0fee4d433452ac3ba825047aec8d56590b
Rati Sonkar <73036667+rati630@users.noreply.github.com>
2022-07-22 13:00:18 +0530

    Update render.js

--- New folder/server/services/render.js
+++ New folder/server/services/render.js
@@ -1,5 +1,18 @@
+const axios = require('axios');
+
+
+
+
+
 exports.homeRoutes =(req,res)=>{
-    res.render('index');
+    //Make a get request to apli/users
+    axios.get('http://localhost:3000/api/users')
+    .then(function(response){
+        res.render('index', { users : response.data });
+    })
+    .catch(err =>{
+        res.send(err);
+    })
 }
 
 exports.add_user=(req,res)=>{
